44095f2b6ed8275f1c3743603381542a7f081f78
Enrique Angrisano <milangrisano@gmail.com>
2024-07-01 21:06:58 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:provider/provider.dart';
+import 'package:vertical_landing_page/providers/page_provider.dart';
+import 'package:vertical_landing_page/router/router.dart';
+ 
+void main() {
+  setUrlStrategy(PathUrlStrategy());
+  runApp(AppState());
+}
+
+class AppState extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => PageProvider() )
+      ],
+      child: MyApp(),
+    );
+  }
+}
+
+
+
+ 
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  void initState() { 
+    super.initState();
+    Flurorouter.configureRoutes();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Landing Page',
+      initialRoute: '/about',
+      onGenerateRoute: Flurorouter.router.generator,
+    );
+  }
+}
